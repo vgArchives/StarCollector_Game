77838f3531f026a96ab72260b678908a3bc2d82d
Victor <81108091+vgArchives@users.noreply.github.com>
2021-11-19 12:05:00 -0300

    Update CollectableSounds.cs

--- ZigZagGame/Assets/Scripts/Collectable/CollectableSounds.cs
+++ ZigZagGame/Assets/Scripts/Collectable/CollectableSounds.cs
@@ -5,19 +5,7 @@ using UnityEngine;
 public class CollectableSounds : MonoBehaviour
 {
     [SerializeField] private AudioClip starSound;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
