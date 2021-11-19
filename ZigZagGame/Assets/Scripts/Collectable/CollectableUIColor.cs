17bf43e5277faf188688dacc1543bee2125270da
Victor <81108091+vgArchives@users.noreply.github.com>
2021-11-19 12:05:22 -0300

    Update CollectableUIColor.cs

--- ZigZagGame/Assets/Scripts/Collectable/CollectableUIColor.cs
+++ ZigZagGame/Assets/Scripts/Collectable/CollectableUIColor.cs
@@ -5,19 +5,7 @@ using UnityEngine;
 public class CollectableUIColor : MonoBehaviour
 {
     [SerializeField] private Renderer collectableLight;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void ChangeColor()
     {
         var colorType = FindObjectOfType<GameManager>().colorType;
