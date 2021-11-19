d9c5ac2bb7c38320d4794137253284b86641bb38
Victor <81108091+vgArchives@users.noreply.github.com>
2021-11-19 12:10:53 -0300

    Update PlayerController.cs

--- ZigZagGame/Assets/Scripts/Player/PlayerController.cs
+++ ZigZagGame/Assets/Scripts/Player/PlayerController.cs
@@ -17,12 +17,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpSpeed = 8f;
     [SerializeField] private float gravity = 10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
